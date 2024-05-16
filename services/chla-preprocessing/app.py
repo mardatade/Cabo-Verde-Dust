76a8ea15ef51065f9a774f9cc0968291a364a189
Willi Rath <wrath@geomar.de>
2024-05-16 16:25:04 +0200

    Add cmems auth

--- services/chla-preprocessing/app.py
+++ services/chla-preprocessing/app.py
@@ -1,6 +1,7 @@
 import copernicusmarine
 
 import click
+import os
 
 dataset_id = "cmems_obs-oc_glo_bgc-plankton_nrt_l4-gapfree-multi-4km_P1D"
 output_name = dataset_id + ".nc"
@@ -8,8 +9,16 @@ lon_bounds = (-28, -12)
 lat_bounds = (8, 22)
 
 
+CMEMS_USER = os.environ["CMEMS_USER"]
+CMEMS_PASS = os.environ["CMEMS_PASS"]
+
+
 def load_write_CHL(dataset_id=None, lon_bounds=None, lat_bounds=None, output_name=None):
-    chldataset = copernicusmarine.open_dataset(dataset_id=dataset_id)
+    chldataset = copernicusmarine.open_dataset(
+        dataset_id=dataset_id,
+        username=CMEMS_USER,
+        password=CMEMS_PASS,
+    )
 
     chldataset = chldataset.sel(
         longitude=slice(*lon_bounds), latitude=slice(*lat_bounds)
